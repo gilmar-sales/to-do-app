d7b1e5a6f6c0bb5f291533395230d7d86bf74688
gilmarxd <gilmar_custodio_@hotmail.com>
2021-03-03 11:05:54 -0300

    :sparkles: create custom logger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
+import { MyLogger } from '@config';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { logger: MyLogger });
+
+  await app.listen(3000, () => {
+    Logger.log(`Server running on http://localhost:3000/`);
+  });
 }
 bootstrap();
