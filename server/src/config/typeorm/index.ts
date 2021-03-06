851f94e266fc14d38bf538b7febe422c73fbde14
gilmarxd <gilmar_custodio_@hotmail.com>
2021-03-06 08:55:09 -0300

    :recycle: update typeorm config

--- server/src/config/typeorm/index.ts
+++ server/src/config/typeorm/index.ts
@@ -1,17 +1,18 @@
 import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { join, resolve } from 'path';
 
-import { TYPEORM } from '@environments';
+import { TYPEORM } from '@environments/index';
 
 export class TypeOrmService implements TypeOrmOptionsFactory {
   async createTypeOrmOptions(): Promise<TypeOrmModuleOptions> {
     const options: TypeOrmModuleOptions = {
       ...TYPEORM,
       type: 'postgres',
+      synchronize: true,
       entities: [
-        resolve(__dirname, '..', '..', '**', 'entities', '*.entity.{ts,js}'),
+        resolve(__dirname, '..', '..', 'entities', '*.entity.{ts,js}'),
       ],
-      migrations: [resolve(__dirname, '..', 'migrations', '*.{ts,js}')],
+      migrations: [resolve(__dirname, '..', '..', 'migrations', '*.{ts,js}')],
       cli: {
         migrationsDir: join('src', 'migrations'),
       },
